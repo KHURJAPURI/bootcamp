7bd0bbf2db6ef04d64d3f40265e3f3c419a6cc5f
KHURJAPURI <181564866+KHURJAPURI@users.noreply.github.com>
2025-02-04 10:20:25 +0530

    Add files via upload

---
+++ chaacte.c
@@ -0,0 +1,17 @@
+//character is vowal, consonent and char.
+#include<stdio.h>
+int main()
+{
+	char ch={a,e,i,o,u};
+	char c;
+		printf("Enter the character:");
+	scanf("%c",&c);
+	if(c==ch)
+	{
+		printf("vowal");
+	}
+	else
+	{
+		printf("consonant");
+	}
+}
