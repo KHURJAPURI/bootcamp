cefa1fcc4975fd9e67b1039ea0cf773d6e8fa99b
KHURJAPURI <181564866+KHURJAPURI@users.noreply.github.com>
2025-02-04 12:02:54 +0530

    Add files via upload

---
+++ structer.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+struct book
+{
+	int book;
+    int price; 
+    int pages;
+    int id;
+};
+int main()
+{ struct book e[5];
+int i;
+for(i=0;i<5;i++){
+
+    printf("\enter the price of book");
+    scanf("%d",&e[i].book);
+    printf("\n enter the page of book ");
+    scanf("%d",&e[i].price);
+    printf("\n enter the id of book");
+    scanf("%d",&e[i].id);
+ }   for(i=0;i<5;i++){
+ 	averageprice=averageprise+b[i].price;
+ }
+ avareg
+ return 0;
+}
